088fe852f7a9ae2f5dcffbf522603afc2d2640f9
vacationjunkmail <vacationjunkmail@gmail.com>
2024-09-06 18:03:57 -0400

    adding

--- cpp/cpp24hrs/ForLoop/main.cpp
+++ cpp/cpp24hrs/ForLoop/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
+#include <cstdio>
+
+//https://www.geeksforgeeks.org/cpp-cstdio/
+//https://www.geeksforgeeks.org/cpp-printf-function/
 
 int main()
 {
     int production[] = {1999,2000,2001,2002,2003,2004};
     for(int year : production){
         std::cout << "New for loop style in C++: "<< year << "\n";
+        // std::printf("\tPrint printf: %d\n", production[i]);
     }
+
+
     const int production_size = sizeof(production)/sizeof(production[0]);
-    std::cout << production_size << std::endl;
+    std::cout << "\nSize of production array:" << production_size << std::endl;
     for(int i = 0; i < production_size; i++ )
     {
         std::cout << "Old for loop style in C++: " << production[i] << "\n";
     }
     //std::cout << std::endl;
+
+    std::cout << "\nUsing printf" << "\n";
+    for(int i = 0; i < production_size; i++ )
+    {
+        std::printf("\tOld for loopy style using printf: %d\n", production[i]);
+    }
+
     return 0;
 }
