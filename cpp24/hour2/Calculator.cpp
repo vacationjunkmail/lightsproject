12ad3d181307bcceae577bdd8f928810fe43ba9d
vacationjunkmail <vacationjunkmail@gmail.com>
2024-09-26 18:23:24 -0400

    Changing Cal app

--- cpp24/hour2/Calculator.cpp
+++ cpp24/hour2/Calculator.cpp
@@ -12,17 +12,16 @@ int main()
 		{777,9311},
 		{3,0}
 		};
+	
+	// Array to store values to add.	
 	int addArray[2];
 	
 	std::cout << "Running calculator....\n";
-	//std::cout << "\t"<<numArray[0][0]<<" + "<<numArray[0][1]<<" = " << add(numArray[0][0],numArray[0][1])<< "\n";
 	
 	for (int a = 0; a < 3; a++) {
 		for (int b = 0; b < 2; b++) {
 			addArray[b] = numArray[a][b];
 		}
 		std::cout << "\t"<<addArray[0]<<" + "<<addArray[1]<<" = " << add(addArray[0],addArray[1])<< "\n";
-		//std::cout << "\t" << add(addArray[0],addArray[1])<<"\n"; 
 	}
-	
 }
