38606df764ef681088319a7757c228c7d8b64c59
vacationjunkmail <vacationjunkmail@gmail.com>
2024-09-29 15:18:04 -0400

    removed motto.cpp added rectang.cpp

---
+++ cpp24/hour3/Rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	unsigned short area, width = 26, length;
+	length = 30;
+	area = width * length;
+	std::cout << "width:" << width <<"\n";
+	std::cout << "length:" << length <<"\n";
+	std::cout << "area:" << area << "\n";	
+	return 0;	
+}
