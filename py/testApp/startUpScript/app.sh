f50886f249ba2a853bc739d5962d28fd23e6bb97
vacationjunkmail <vacationjunkmail@gmail.com>
2024-11-27 08:33:41 -0500

    shell file wasn't complete with last push

--- py/testApp/startUpScript/app.sh
+++ py/testApp/startUpScript/app.sh
@@ -2,7 +2,7 @@
 # https://www.tutorialspoint.com/How-to-set-python-environment-variable-PYTHONPATH-on-Linux
 # https://linuxize.com/post/how-to-run-linux-commands-in-background/
 export PYTHONPATH="/home/pi/code/py/testApp:$"
-/home/pi/code/py/testApp/appOne/appOne.py #> /dev/null 2>&1 &
-/home/pi/code/py/testApp/appTwo/appTwo.py #> /dev/null 2>&1 & 
+/home/pi/code/py/testApp/appOne/appOne.py > /dev/null 2>&1 &
+/home/pi/code/py/testApp/appTwo/appTwo.py > /dev/null 2>&1 & 
 
 
