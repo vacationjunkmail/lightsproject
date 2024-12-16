6c3e350386698e1613204054bd445234cdc2757b
vacationjunkmail <vacationjunkmail@gmail.com>
2024-12-15 19:43:14 -0500

    Working on changing code over to a class

--- py/monitorFiles/mf.py
+++ py/monitorFiles/mf.py
@@ -5,6 +5,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
 import subprocess
+import sys
 
 def check_directory(directory):
     for f in directory.iterdir():
@@ -22,18 +23,32 @@ def move_file(event):
 def on_created(event):
     print(f"Event:{event.src_path}")
     move_file(event)
-if __name__ == "__main__":
+
+class Monitor_Directory(FileSystemEventHandler):
+
+    def __init__(self,sourceDir = '/home/pi/Desktop/source',destinationDir = '/home/pi/Desktop/destination'):
+        self.sourceDir= sourceDir
+        self.destinationDir = destinationDir
     
+    def sourceDir(self):
+        print(f"source:{self.sourceDir}")
+
+    def destinationDir(self):
+        print(f"destination:{self.destinationDir}")
+        
+if __name__ == "__main__":
+
+    m = Monitor_Directory()
+    #m.sourceDir = 'Here we go'
+    print(m.sourceDir)
+    print(m.destinationDir)
+    
+    sys.exit()    
     sourceDir = Path('/home/pi/Desktop/source')
     global destinationDir
     destinationDir = Path('/home/pi/Desktop/destination')
 
-    #for item in sourceDir.iterdir():
-    #    print(item)
-
     event_handler = FileSystemEventHandler()
-    # Prints all methods in a class
-    #print(dir(event_handler))
 
     #Custom on_created
     event_handler.on_created = on_created
